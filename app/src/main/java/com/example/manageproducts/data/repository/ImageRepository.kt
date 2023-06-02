af384bd9e19fdbe9804c408efffbabba2fb4d383
Hieu Vu <star.hieu.99@gmail.com>
2023-06-02 22:02:46 +0700

    Implement repository

--- app/src/main/java/com/example/manageproducts/data/repository/ImageRepository.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/ImageRepository.kt
@@ -1,4 +1,5 @@
 package com.example.manageproducts.data.repository
 
 interface ImageRepository {
+    suspend fun uploadImage(image: ByteArray)
 }
--- app/src/main/java/com/example/manageproducts/data/repository/impl/ImageRepositoryImpl.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/impl/ImageRepositoryImpl.kt
@@ -1,4 +1,17 @@
-package com.example.manageproducts.data.repository
+package com.example.manageproducts.data.repository.impl
 
-class ImageRepositoryImpl {
+import com.example.manageproducts.data.repository.ImageRepository
+import io.github.jan.supabase.storage.Storage
+import io.github.jan.supabase.storage.upload
+import javax.inject.Inject
+
+class ImageRepositoryImpl @Inject constructor(
+    private val storage: Storage
+) : ImageRepository {
+    override suspend fun uploadImage(image: ByteArray) {
+        storage["Product Image"].update(
+            "Product Image", image,
+            upsert = true
+        )
+    }
 }
