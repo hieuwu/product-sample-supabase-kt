33691099cda049f9c3bdd47e38ffd852e4a1816c
Hieu Vu <star.hieu.99@gmail.com>
2023-06-04 23:34:49 +0700

    Return type

--- app/src/main/java/com/example/manageproducts/data/repository/impl/ImageRepositoryImpl.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/impl/ImageRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.manageproducts.data.repository.impl
 
+import com.example.manageproducts.BuildConfig
 import com.example.manageproducts.data.repository.ImageRepository
 import io.github.jan.supabase.storage.Storage
 import javax.inject.Inject
@@ -7,11 +8,17 @@ import javax.inject.Inject
 class ImageRepositoryImpl @Inject constructor(
     private val storage: Storage
 ) : ImageRepository {
-    override suspend fun uploadImage(fileName: String, image: ByteArray) {
-        storage["Product%20Image"].upload(path = "$fileName.png", data = image, upsert = false)
+    override suspend fun uploadImage(fileName: String, image: ByteArray): String {
+        val result =
+            storage["Product%20Image"].upload(path = "$fileName.png", data = image, upsert = false)
+        return result
     }
 
     override suspend fun getImage() {
         TODO("Not yet implemented")
     }
+
+    private fun buildImageUrl(imageFileName: String) =
+        "${BuildConfig.SUPABASE_URL}/storage/v1/object/public/"
+
 }
