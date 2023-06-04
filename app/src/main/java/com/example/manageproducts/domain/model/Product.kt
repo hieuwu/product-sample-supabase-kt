1b1428ed20dbc652400bae839789e3a4f02082a9
Hieu Vu <star.hieu.99@gmail.com>
2023-06-04 23:34:24 +0700

    Update object and mapper

--- app/src/main/java/com/example/manageproducts/domain/model/Product.kt
+++ app/src/main/java/com/example/manageproducts/domain/model/Product.kt
@@ -4,4 +4,5 @@ data class Product(
     val id: String,
     val name: String,
     val price: Double,
+    val image: String = ""
 )
--- app/src/main/java/com/example/manageproducts/domain/usecase/impl/GetProductDetailsUseCaseImpl.kt
+++ app/src/main/java/com/example/manageproducts/domain/usecase/impl/GetProductDetailsUseCaseImpl.kt
@@ -10,11 +10,13 @@ class GetProductDetailsUseCaseImpl @Inject constructor(
 ) : GetProductDetailsUseCase {
     override suspend fun execute(input: GetProductDetailsUseCase.Input): GetProductDetailsUseCase.Output {
         val result = productRepository.getProduct(input.id)
-        return GetProductDetailsUseCase.Output.Success(data = Product(
-            id = result.id ?: "",
-            name = result.name,
-            price = result.price,
-        )
+        return GetProductDetailsUseCase.Output.Success(
+            data = Product(
+                id = result.id ?: "",
+                name = result.name,
+                price = result.price,
+                image = result.image ?: "",
+            )
         )
     }
 }
