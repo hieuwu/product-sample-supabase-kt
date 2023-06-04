610b53016e839d41b5459622b9c0a13adf06d6b3
Hieu Vu <star.hieu.99@gmail.com>
2023-06-04 23:34:14 +0700

    Add product image

--- app/src/main/java/com/example/manageproducts/domain/usecase/impl/UpdateProductUseCaseImpl.kt
+++ app/src/main/java/com/example/manageproducts/domain/usecase/impl/UpdateProductUseCaseImpl.kt
@@ -8,7 +8,10 @@ class UpdateProductUseCaseImpl @Inject constructor(
     private val productRepository: ProductRepository
 ) : UpdateProductUseCase {
     override suspend fun execute(input: UpdateProductUseCase.Input): UpdateProductUseCase.Output {
-        productRepository.updateProduct(id = input.id, name = input.name, price = input.price)
+        productRepository.updateProduct(
+            id = input.id, name = input.name, price = input.price,
+            imageName = input.imageName, imageFile = input.imageFile
+        )
         return UpdateProductUseCase.Output.Success
     }
 }
--- app/src/main/java/com/example/manageproducts/domain/usecase/impl/UploadImageUseCaseImpl.kt
+++ app/src/main/java/com/example/manageproducts/domain/usecase/impl/UploadImageUseCaseImpl.kt
@@ -1,14 +1,20 @@
 package com.example.manageproducts.domain.usecase.impl
 
+import android.util.Log
 import com.example.manageproducts.data.repository.ImageRepository
 import com.example.manageproducts.domain.usecase.UploadImageUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UploadImageUseCaseImpl @Inject constructor(
     private val imageRepository: ImageRepository
-): UploadImageUseCase {
+) : UploadImageUseCase {
     override suspend fun execute(input: UploadImageUseCase.Input): UploadImageUseCase.Output {
-        imageRepository.uploadImage(input.fileName, input.imageByteArray)
-        return UploadImageUseCase.Output.Success
+        return withContext(Dispatchers.IO) {
+            val url = imageRepository.uploadImage(input.fileName, input.imageByteArray)
+            Log.d("UploadImageUseCase", url)
+            UploadImageUseCase.Output.Success()
+        }
     }
 }
