960a0efae2b5e7b872b0897ada185e9e7569dae3
Hieu Vu <star.hieu.99@gmail.com>
2023-06-21 22:31:17 +0700

    Create sign up use case

--- app/src/main/java/com/example/manageproducts/domain/usecase/SignUpUseCase.kt
+++ app/src/main/java/com/example/manageproducts/domain/usecase/SignUpUseCase.kt
@@ -1,4 +1,9 @@
 package com.example.manageproducts.domain.usecase
 
-interface SignUpUseCase {
+interface SignUpUseCase: UseCase<SignUpUseCase.Input, SignUpUseCase.Output> {
+    class Input(val email: String, val password: String)
+    sealed class Output {
+        object Success: Output()
+        object Failure: Output()
+    }
 }
--- app/src/main/java/com/example/manageproducts/domain/usecase/impl/SignUpUseCaseImpl.kt
+++ app/src/main/java/com/example/manageproducts/domain/usecase/impl/SignUpUseCaseImpl.kt
@@ -1,4 +1,21 @@
 package com.example.manageproducts.domain.usecase.impl
 
-class SignUpUseCaseImpl {
+import com.example.manageproducts.data.repository.AuthenticateRepository
+import com.example.manageproducts.domain.usecase.SignUpUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SignUpUseCaseImpl @Inject constructor(
+    private val authenticateRepository: AuthenticateRepository
+) : SignUpUseCase {
+    override suspend fun execute(input: SignUpUseCase.Input): SignUpUseCase.Output =
+        withContext(Dispatchers.IO) {
+            val result = authenticateRepository.signUp(input.email, input.password)
+            if (result) {
+                SignUpUseCase.Output.Success
+            } else {
+                SignUpUseCase.Output.Failure
+            }
+        }
 }
