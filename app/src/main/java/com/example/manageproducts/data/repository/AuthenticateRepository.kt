cc5dcfc05e99825848bc6a080291599287d9df7a
Hieu Vu <star.hieu.99@gmail.com>
2023-06-21 22:31:30 +0700

    Implement repository

--- app/src/main/java/com/example/manageproducts/data/repository/AuthenticateRepository.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/AuthenticateRepository.kt
@@ -1,4 +1,6 @@
 package com.example.manageproducts.data.repository
 
 interface AuthenticateRepository {
+    suspend fun logIn(email: String, password: String): Boolean
+    suspend fun signUp(email: String, password: String): Boolean
 }
--- app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticateRepositoryImpl.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticateRepositoryImpl.kt
@@ -1,4 +1,26 @@
 package com.example.manageproducts.data.repository.impl
 
-class AuthenticateRepositoryImpl {
+import com.example.manageproducts.data.repository.AuthenticateRepository
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.gotrue.providers.builtin.Email
+import javax.inject.Inject
+
+class AuthenticateRepositoryImpl @Inject constructor(
+    private val authService: GoTrue
+) : AuthenticateRepository {
+    override suspend fun logIn(email: String, password: String): Boolean {
+        authService.loginWith(Email) {
+            this.email = email
+            this.password = password
+        }
+        return true
+    }
+
+    override suspend fun signUp(email: String, password: String): Boolean {
+        authService.signUpWith(Email) {
+            this.email = email
+            this.password = password
+        }
+        return true
+    }
 }
