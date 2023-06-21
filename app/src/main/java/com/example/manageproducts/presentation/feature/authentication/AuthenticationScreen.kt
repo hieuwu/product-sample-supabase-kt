06f3bc6e14676c8d220a842c5aec73bd22698b98
Hieu Vu <star.hieu.99@gmail.com>
2023-06-21 22:30:54 +0700

    Implement screen and handle state change

--- app/src/main/java/com/example/manageproducts/presentation/feature/authentication/AuthenticationScreen.kt
+++ app/src/main/java/com/example/manageproducts/presentation/feature/authentication/AuthenticationScreen.kt
@@ -1,21 +1,23 @@
 package com.example.manageproducts.presentation.feature.authentication
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.manageproducts.presentation.navigation.AuthenticationFeature
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthenticationScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
+    viewModel: AuthenticationViewModel = hiltViewModel()
 ) {
     Scaffold(
         topBar = {
@@ -30,8 +32,47 @@ fun AuthenticationScreen(
             )
         }
     ) { paddingValues ->
-        Column(modifier = modifier.padding(paddingValues = paddingValues)) {
-
+        Column(modifier = modifier.padding(paddingValues).padding(20.dp)) {
+            val email = viewModel.email.collectAsState(initial = "")
+            val password = viewModel.password.collectAsState()
+            androidx.compose.material.OutlinedTextField(
+                label = {
+                    Text(
+                        text = "Email",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                maxLines = 1,
+                shape = RoundedCornerShape(32),
+                modifier = modifier.fillMaxWidth(),
+                value = email.value,
+                onValueChange = {
+                    viewModel.onEmailChange(it)
+                },
+            )
+            androidx.compose.material.OutlinedTextField(
+                label = {
+                    Text(
+                        text = "Password",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                maxLines = 1,
+                shape = RoundedCornerShape(32),
+                modifier = modifier.fillMaxWidth()
+                    .padding(top = 12.dp),
+                value = password.value,
+                onValueChange = {
+                    viewModel.onPasswordChange(it)
+                },
+            )
+            Button(modifier = modifier.fillMaxWidth().padding(top = 12.dp), onClick = {
+                navController.navigate(AuthenticationFeature.route)
+            }) {
+                Text("Login")
+            }
         }
     }
 }
--- app/src/main/java/com/example/manageproducts/presentation/feature/authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/manageproducts/presentation/feature/authentication/AuthenticationViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.manageproducts.presentation.feature.authentication
 
-class AuthenticationViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthenticationViewModel @Inject constructor(
+
+): ViewModel() {
+
+    private val _email = MutableStateFlow<String>("")
+    val email: Flow<String> = _email
+
+    private val _password = MutableStateFlow("")
+    val password = _password
+
+    fun onEmailChange(email: String) {
+        _email.value = email
+    }
+
+    fun onPasswordChange(password: String) {
+        _password.value = password
+    }
+
+    fun onLogin() {
+
+    }
 }
