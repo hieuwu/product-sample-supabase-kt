a01dd8cf0508144d0119c8380a2298f91991a5f2
Hieu Vu <star.hieu.99@gmail.com>
2023-06-22 21:42:14 +0700

    Implement sign up screen

--- app/src/main/java/com/example/manageproducts/presentation/feature/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/manageproducts/presentation/feature/signup/SignUpScreen.kt
@@ -1,4 +1,87 @@
 package com.example.manageproducts.presentation.feature.signup
 
-class SignUpScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: SignUpViewModel = hiltViewModel()
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colorScheme.primary,
+                title = {
+                    Text(
+                        text = "Sign Up",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+                },
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .padding(20.dp)
+        ) {
+            val email = viewModel.email.collectAsState(initial = "")
+            val password = viewModel.password.collectAsState()
+            OutlinedTextField(
+                label = {
+                    Text(
+                        text = "Email",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                maxLines = 1,
+                shape = RoundedCornerShape(32),
+                modifier = modifier.fillMaxWidth(),
+                value = email.value,
+                onValueChange = {
+                    viewModel.onEmailChange(it)
+                },
+            )
+            OutlinedTextField(
+                label = {
+                    Text(
+                        text = "Password",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                maxLines = 1,
+                shape = RoundedCornerShape(32),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp),
+                value = password.value,
+                onValueChange = {
+                    viewModel.onPasswordChange(it)
+                },
+            )
+            Button(modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 12.dp), onClick = {
+                viewModel.onSignUp()
+            }) {
+                Text("Sign up")
+            }
+        }
+    }
 }
