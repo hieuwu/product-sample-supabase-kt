2fc0216c1582254e9a5a8821d8b99ff275b743be
Hieu Vu <star.hieu.99@gmail.com>
2023-07-09 19:56:53 +0700

    Implement auth

--- app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticationRepositoryImpl.kt
@@ -1,12 +1,16 @@
 package com.example.manageproducts.data.repository.impl
 
+import android.content.Context
 import com.example.manageproducts.data.repository.AuthenticationRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.gotrue.providers.Google
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import javax.inject.Inject
 
 class AuthenticationRepositoryImpl @Inject constructor(
-    private val goTrue: GoTrue
+    private val goTrue: GoTrue,
+    @ApplicationContext context: Context,
 ) : AuthenticationRepository {
     override suspend fun signIn(email: String, password: String): Boolean {
         return try {
@@ -26,6 +30,15 @@ class AuthenticationRepositoryImpl @Inject constructor(
                 this.email = email
                 this.password = password
             }
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override suspend fun signInWithGoogle(): Boolean {
+        return try {
+            goTrue.loginWith(Google, redirectUrl = "https://supabase.co" )
             true
         } catch (e: Exception) {
             false
