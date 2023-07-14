d6ba7f88d7a4e39d7f1d445a134400357d4eac57
Hieu Vu <star.hieu.99@gmail.com>
2023-07-14 22:52:17 +0700

    Place holder Ui

--- app/src/main/java/com/example/manageproducts/presentation/feature/signin/SignInSuccessScreen.kt
+++ app/src/main/java/com/example/manageproducts/presentation/feature/signin/SignInSuccessScreen.kt
@@ -1,2 +1,22 @@
 package com.example.manageproducts.presentation.feature.signin
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun SignInSuccessScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    email: String,
+    createdAt: String,
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(text = "Sign in successfully!")
+        Text(text = "Email $email")
+        Text(text = "Created at $createdAt")
+    }
+}
