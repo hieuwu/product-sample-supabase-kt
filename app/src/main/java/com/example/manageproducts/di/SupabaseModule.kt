791e42b1d6873b3e8635613a14516f2f20018b09
Hieu Vu <star.hieu.99@gmail.com>
2023-07-14 22:00:08 +0700

    Update appp link scheme

--- app/src/main/java/com/example/manageproducts/di/SupabaseModule.kt
+++ app/src/main/java/com/example/manageproducts/di/SupabaseModule.kt
@@ -1,20 +1,12 @@
 package com.example.manageproducts.di
 
-import android.content.Context
 import com.example.manageproducts.BuildConfig
-import com.example.manageproducts.R
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.annotiations.SupabaseExperimental
 import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.FlowType
 import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.gotrue.gotrue
 import io.github.jan.supabase.postgrest.Postgrest
@@ -28,7 +20,6 @@ import javax.inject.Singleton
 @Module
 object SupabaseModule {
 
-    @OptIn(SupabaseExperimental::class)
     @Provides
     @Singleton
     fun provideSupabaseClient(): SupabaseClient {
@@ -38,9 +29,8 @@ object SupabaseModule {
         ) {
             install(Postgrest)
             install(GoTrue) {
-                flowType = FlowType.PKCE
-                host = "gizmos"
-                scheme = "example"
+                scheme = "app"
+                host = "supabase.com"
             }
             install(Storage)
         }
