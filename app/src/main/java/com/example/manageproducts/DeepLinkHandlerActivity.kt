655ed5644e2fc74304c7f6be06beda2fa45d333e
Hieu Vu <star.hieu.99@gmail.com>
2023-07-15 15:11:20 +0700

    Create deeplink handler

---
+++ app/src/main/java/com/example/manageproducts/DeepLinkHandlerActivity.kt
@@ -0,0 +1,43 @@
+package com.example.manageproducts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.manageproducts.ui.theme.ManageProductsTheme
+
+class DeepLinkHandlerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ManageProductsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    ManageProductsTheme {
+        Greeting2("Android")
+    }
+}
