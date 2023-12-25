85da48b0e5a06dd3d053a67a2cb22d83a5bcbf2b
Hieu Vu <star.hieu.99@gmail.com>
2023-12-25 09:27:41 +0700

    Rename variable, remove unused cdoe

--- app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/example/manageproducts/data/repository/impl/AuthenticationRepositoryImpl.kt
@@ -1,20 +1,17 @@
 package com.example.manageproducts.data.repository.impl
 
-import android.content.Context
 import com.example.manageproducts.data.repository.AuthenticationRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.gotrue.providers.Google
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import javax.inject.Inject
 
 class AuthenticationRepositoryImpl @Inject constructor(
-    private val goTrue: Auth,
-    @ApplicationContext context: Context,
+    private val auth: Auth,
 ) : AuthenticationRepository {
     override suspend fun signIn(email: String, password: String): Boolean {
         return try {
-            goTrue.signInWith(Email) {
+            auth.signInWith(Email) {
                 this.email = email
                 this.password = password
             }
@@ -26,7 +23,7 @@ class AuthenticationRepositoryImpl @Inject constructor(
 
     override suspend fun signUp(email: String, password: String): Boolean {
         return try {
-            goTrue.signUpWith(Email) {
+            auth.signUpWith(Email) {
                 this.email = email
                 this.password = password
             }
@@ -38,7 +35,7 @@ class AuthenticationRepositoryImpl @Inject constructor(
 
     override suspend fun signInWithGoogle(): Boolean {
         return try {
-            goTrue.signInWith(Google)
+            auth.signInWith(Google)
             true
         } catch (e: Exception) {
             false
